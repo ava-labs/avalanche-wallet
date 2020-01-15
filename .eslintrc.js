c18f93ac43662ba5198f3c900a4f4df47bd80b13
AVA LABS <ava@AVAs-MacBook-Pro.local>
2020-01-15 13:37:31 -0500

    - console logs can stay on build

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     '@vue/typescript'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
