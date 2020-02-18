8675e9c5e532a93aa9821b04ec1586157bd329dd
AVA LABS <ava@AVAs-MBP.localdomain>
2020-02-18 18:00:15 -0500

    - added favicon images

--- vue.config.js
+++ vue.config.js
@@ -14,12 +14,16 @@ module.exports = {
     }
   },
   pwa: {
+    name: "AVA Wallet",
     manifestOptions: {
       start_url: '/'
     },
     iconPaths: {
-      favicon16: 'favicon.ico',
-      favicon32: 'favicon.ico'
+      favicon16: 'img/icons/favicon-16x16.png',
+      favicon32: 'img/icons/favicon-32x32.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/favicon-32x32.png',
+      msTileImage: 'img/icons/mstile-150x150.png'
     }
   }
 };
