b4feb652e9daa577b9fda8fcedecc73e690c9b5e
AVA LABS <ava@AVAs-MBP.localdomain>
2020-02-19 10:55:49 -0500

    - history mode working again

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.use (function (req, res, next) {
         res.redirect('https://' + req.headers.host + req.url);
     }
 });
+app.use(history());
 
 // Serving Static Files
 app.use(express.static('dist'));
-app.use(history());
 
 
 
