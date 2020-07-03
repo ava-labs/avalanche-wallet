d885836fd6949fd97444bd404c61065c7f5a24d2
Lionell Briones <llenoil@gmail.com>
2020-07-03 11:47:51 +0800

    Added torus direct web sdk

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -5,7 +5,14 @@
                 <div v-if="!newPrivateKey">
                     <h1>Create Wallet</h1>
                     <p>Generate a new private key to use with your wallet.</p>
-                    <button class="generate" @click="createKey">Generate Private Key</button>
+                    <div class="generate-container">
+                        <div>
+                            <button class="generate" @click="createKey">Generate Private Key</button>
+                        </div>
+                        <div>
+                            <button class="generate" @click="createKeyWithGoogle">Create with Google</button>
+                        </div>
+                    </div>
                     <router-link to="/access">Already have a wallet?</router-link>
                 </div>
                 <div v-else>
@@ -23,6 +30,7 @@
     import TextDisplayCopy from "../components/misc/TextDisplayCopy";
     import {keyChain} from "../AVA";
     import RememberKey from "@/components/misc/RememberKey";
+    import TorusSdk from "@toruslabs/torus-direct-web-sdk";
 
     export default {
         components: {
@@ -48,10 +56,40 @@
                 let privk = keypair.getPrivateKey(); //returns Buffer
                 let privkstr = keypair.getPrivateKeyString(); //returns an AVA serialized string
 
+                console.log('getPublicKeyString', pubkstr)
+                console.log('getPrivateKeyString', privkstr)
+                
                 // console.log(keypair.getAddressString());
                 this.newAddr = keypair.getAddressString();
                 this.newPrivateKey = privkstr;
                 this.newPublicKey = pubkstr;
+            },
+
+            async createKeyWithGoogle() {
+                const torusdirectsdk = new TorusSdk({
+                    baseUrl: `${location.origin}/serviceworker`,
+                    enableLogging: true,
+                    proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
+                    network: "ropsten", // details for test net
+                });
+                
+                await torusdirectsdk.init();
+
+                const loginDetails = await torusdirectsdk.triggerLogin({
+                    typeOfLogin: "google",
+                    verifier: "ava-google",
+                    clientId: "74915647456-4ctjtqo7rb8kgn9qib30dia79a20pvdb.apps.googleusercontent.com",
+                });
+
+                console.log('loginDetails.privateKey', loginDetails)
+
+                try{
+                    let res = await this.$store.dispatch('accessWallet', loginDetails.privateKey);
+                }catch (e) {
+                    console.error('error', e)
+                    // this.error = 'Invalid Private Key';
+                    // this.isLoading = false;
+                }
             },
 
             access(){
@@ -72,9 +110,18 @@
         text-align: center;
     }
 
+    .generate-container {
+        display: flex;
+        margin: 0 -15px;
+        padding: 30px 0;
+        & > div {
+            padding: 0 15px;
+        }
+    }
+
     .generate{
         display: block;
-        margin: 30px auto;
+        /* margin: 30px auto; */
         background-color: #000;
         padding: 8px 18px;
         border-radius: 6px;
--- src/views/access/Menu.vue
+++ src/views/access/Menu.vue
@@ -13,10 +13,49 @@
                 <p>AVA_ZvtFT.json</p>
                 <router-link to="/access/keystore"></router-link>
             </div>
+            <div class="option" @click="loginWithGoogle">
+                <h4>Google</h4>
+            </div>
         </div>
         <router-link to="/create">Don't have a wallet?</router-link>
     </div>
 </template>
+
+<script>
+import TorusSdk from "@toruslabs/torus-direct-web-sdk";
+
+export default {
+    methods: {
+        async loginWithGoogle() {
+            const torusdirectsdk = new TorusSdk({
+                baseUrl: `${location.origin}/serviceworker`,
+                enableLogging: true,
+                proxyContractAddress: "0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183", // details for test net
+                network: "ropsten", // details for test net
+            });
+            
+            await torusdirectsdk.init();
+
+            const loginDetails = await torusdirectsdk.triggerLogin({
+                typeOfLogin: "google",
+                verifier: "ava-google",
+                clientId: "74915647456-4ctjtqo7rb8kgn9qib30dia79a20pvdb.apps.googleusercontent.com",
+            });
+
+            console.log('loginDetails.privateKey', loginDetails)
+
+            try{
+                let res = await this.$store.dispatch('accessWallet', loginDetails.privateKey);
+            }catch (e) {
+                console.error('error', e)
+                // this.error = 'Invalid Private Key';
+                // this.isLoading = false;
+            }
+        }
+    }
+}
+</script>
+
 <style scoped lang="scss">
     @use '../../main';
 
@@ -24,7 +63,7 @@
     .options{
         margin: 30px auto;
         display: grid;
-        grid-template-columns: $col_w $col_w;
+        grid-template-columns: $col_w $col_w $col_w;
         grid-gap: 30px;
 
         >div{
@@ -53,6 +92,7 @@
         box-shadow: 4px 3px 10px rgba(0,0,0,0.2);
         border-radius: 4px;
         padding: 30px;
+        cursor: pointer;
 
         a{
             position: absolute;
