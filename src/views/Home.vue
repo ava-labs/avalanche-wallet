030a18d84539c6f962a12e8373c765a09432e4f8
Emre Kanatli <kanatliemre@gmail.com>
2020-08-20 15:09:49 -0400

    - bump vuetify - mini css update login options

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -88,6 +88,7 @@ export default class Home extends Vue {}
             .login_option {
                 display: flex;
                 flex-direction: column;
+                border-radius: 2px;
                 align-items: flex-start;
                 justify-content: space-between;
                 background-color: var(--bg-light);
