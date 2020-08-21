01218d4fe6cd34c498174055c0a16353217711b9
Emre Kanatli <kanatliemre@gmail.com>
2020-08-21 16:54:45 -0400

    -rename portfolio spec - improve transfer spec

--- cypress/integration/portfolio_spec.ts
+++ cypress/integration/portfolio_spec.ts
@@ -2,6 +2,7 @@ import {faucetAddress, userKey0, sendAvax, TEST_MNEMONIC, createFixedCapAsset} f
 import {getNFT} from "../support/nftHelper";
 
 describe('Wallet', () => {
+
     before(()=>{
         cy.wait(1000);
         cy.enterWallet(TEST_MNEMONIC);
@@ -11,41 +12,30 @@ describe('Wallet', () => {
     after(() => {
     })
 
-    it('can display fungible', async () => {
+    it('can display fungible', () => {
         const ADDR_0 = userKey0.getAddressString();
-
-        console.log("SEND TO: ",ADDR_0)
-        await sendAvax(ADDR_0, 1)
-        cy.wait(1000);
+        sendAvax(ADDR_0, 1)
+        cy.wait(2000);
         cy.get('.refresh button').click();
+        cy.wait(1500);
         cy.get('[data-cy=wallet_balance]').should('have.text', '0.000000001 AVAX')
     })
 
-    it('can display custom fixed cap asset', async () => {
+    it('can display custom fixed cap asset', () => {
         const ADDR_0 = userKey0.getAddressString();
-        await createFixedCapAsset("Fixed Cap Test", "TEST", ADDR_0,1000);
+        createFixedCapAsset("Fixed Cap Test", "TEST", ADDR_0,1000);
         cy.wait(1000);
         cy.get('.refresh button').click();
         cy.wait(1000);
         cy.get(".fungibles_view .asset").should('have.length', 2)
     })
 
-    it('can display NFT', async () => {
+    it('can display NFT', () => {
         const ADDR_0 = userKey0.getAddressString();
-        await getNFT('Test NFT', 'NFT', ADDR_0, 'utf8');
-        cy.wait(5000)
+        getNFT('Test NFT', 'NFT', ADDR_0, 'utf8');
+        cy.wait(2000)
         cy.get("[data-cy=wallet_nft]").click();
         cy.get('.refresh button').click();
 
     });
-
-        // it('can send single fungible', () => {
-    //     cy.get("[data-cy=wallet_transfer]").click();
-    //     cy.get('.max_in_cont .bigIn').type("{uparrow}")
-    //     cy.get('.qr_input .pk_in').type(faucetAddress)
-    //     cy.get('.checkout button').click();
-    //     cy.wait(1000);
-    //     cy.get('.refresh button').click();
-    //     cy.get('[data-cy=wallet_balance]').should('have.text', '0 AVAX')
-    // })
 });
