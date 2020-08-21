5560886e4344694b628bd727050d62c0e99826fc
Emre Kanatli <kanatliemre@gmail.com>
2020-08-21 15:51:26 -0400

    no max bug fixed

--- src/components/misc/CurrencyInputDropdown.vue
+++ src/components/misc/CurrencyInputDropdown.vue
@@ -15,7 +15,7 @@
     import {Vue, Component, Prop, Emit, Watch} from 'vue-property-decorator';
 
     import BN from "bn.js";
-    import Big from 'big.js';
+    // import Big from 'big.js';
     import Dropdown from "@/components/misc/Dropdown.vue";
     // import BigNumInput from "@/components/misc/BigNumInput";
 
@@ -173,7 +173,7 @@
                 let fee = avm.getFee();
                 // console.log(fee);
                 if(fee.gte(balance.amount)){
-                    return null;
+                    return new BN(0);
                 }else{
                     return balance.amount.sub(fee);
                 }
