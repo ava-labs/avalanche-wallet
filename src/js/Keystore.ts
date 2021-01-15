adfc4a04932cf1d748f6a6b494858f067a41a370
Emre Kanatli <kanatliemre@gmail.com>
2021-01-15 13:17:46 -0500

    set ground for refactoring

--- src/js/Keystore.ts
+++ src/js/Keystore.ts
@@ -25,12 +25,138 @@ interface PKCrypt {
     ciphertext: Buffer
 }
 
+async function readV2(data: KeyFile, pass: string) {}
+async function readV3(data: KeyFile, pass: string) {}
+async function readV4(data: KeyFile, pass: string) {}
+
+// Extract old version support out and onto other functions
+async function readV5(data: KeyFile, pass: string) {
+    const version: string = data.version
+
+    if (!SUPPORTED_VERSION.includes(version)) {
+        throw 'INVALID_VERSION'
+    }
+
+    if (version === '2.0') {
+        cryptoHelpers.keygenIterations = ITERATIONS_V2
+    } else {
+        cryptoHelpers.keygenIterations = ITERATIONS_V3
+    }
+
+    let salt: Buffer = bintools.cb58Decode(data.salt)
+    //@ts-ignore
+    let pass_hash: string = data.pass_hash
+
+    let checkHashString: string
+    if (version === '2.0') {
+        let checkHash: Buffer = await cryptoHelpers._pwcleaner(pass, salt)
+        checkHashString = bintools.cb58Encode(checkHash)
+    } else {
+        let checkHash: IHash = await cryptoHelpers.pwhash(pass, salt)
+        checkHashString = bintools.cb58Encode(checkHash.hash)
+    }
+
+    if (checkHashString !== pass_hash) {
+        throw 'INVALID_PASS'
+    }
+
+    let keys: KeyFileKey[] = data.keys
+    let keysDecrypt: KeyFileKeyDecrypted[] = []
+
+    for (let i: number = 0; i < keys.length; i++) {
+        let key_data: KeyFileKey = keys[i]
+
+        let key: Buffer = bintools.cb58Decode(key_data.key)
+        let nonce: Buffer = bintools.cb58Decode(key_data.iv)
+
+        let key_decrypt: Buffer = await cryptoHelpers.decrypt(pass, key, salt, nonce)
+        let key_string: string
+
+        //  versions below 5.0 used private key that had to be cb58 encoded
+        if (['2.0', '3.0', '4.0'].includes(version)) {
+            key_string = bintools.cb58Encode(key_decrypt)
+        } else {
+            key_string = key_decrypt.toString()
+        }
+
+        keysDecrypt.push({
+            key: key_string,
+        })
+    }
+
+    return {
+        version: version,
+        keys: keysDecrypt,
+    }
+}
+
+async function readV6(data: KeyFile, pass: string): Promise<KeyFileDecrypted> {
+    const version: string = data.version
+
+    if (!SUPPORTED_VERSION.includes(version)) {
+        throw 'INVALID_VERSION'
+    }
+
+    if (version === '2.0') {
+        cryptoHelpers.keygenIterations = ITERATIONS_V2
+    } else {
+        cryptoHelpers.keygenIterations = ITERATIONS_V3
+    }
+
+    let salt: Buffer = bintools.cb58Decode(data.salt)
+
+    let checkHashString: string
+    if (version === '2.0') {
+        let checkHash: Buffer = await cryptoHelpers._pwcleaner(pass, salt)
+        checkHashString = bintools.cb58Encode(checkHash)
+    } else {
+        let checkHash: IHash = await cryptoHelpers.pwhash(pass, salt)
+        checkHashString = bintools.cb58Encode(checkHash.hash)
+    }
+
+    let keys: KeyFileKey[] = data.keys
+    let keysDecrypt: KeyFileKeyDecrypted[] = []
+
+    for (let i: number = 0; i < keys.length; i++) {
+        let key_data: KeyFileKey = keys[i]
+
+        let key: Buffer = bintools.cb58Decode(key_data.key)
+        let nonce: Buffer = bintools.cb58Decode(key_data.iv)
+        let key_decrypt: Buffer
+
+        try {
+            key_decrypt = await cryptoHelpers.decrypt(pass, key, salt, nonce)
+        } catch (e) {
+            throw 'INVALID_PASS'
+        }
+        let key_string: string
+
+        //  versions below 5.0 used private key that had to be cb58 encoded
+        if (['2.0', '3.0', '4.0'].includes(version)) {
+            key_string = bintools.cb58Encode(key_decrypt)
+        } else {
+            key_string = key_decrypt.toString()
+        }
+
+        keysDecrypt.push({
+            key: key_string,
+        })
+    }
+
+    return {
+        version: version,
+        keys: keysDecrypt,
+    }
+}
+
 async function readKeyFile(data: KeyFile, pass: string): Promise<KeyFileDecrypted> {
     const version: string = data.version
 
     if (!SUPPORTED_VERSION.includes(version)) {
         throw 'INVALID_VERSION'
     }
+
+    // TODO: Add switch statement for versions and call the correct read functions
 
     if (version === '2.0') {
         cryptoHelpers.keygenIterations = ITERATIONS_V2
