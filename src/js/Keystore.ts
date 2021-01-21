7403fb6033eba4ae0d5916a67450bd4f75004914
plondon <philip.n.london@gmail.com>
2021-01-21 22:01:31 +0100

    Separate extract keys functions

--- src/js/Keystore.ts
+++ src/js/Keystore.ts
@@ -257,6 +257,60 @@ async function readKeyFile(data: AllKeyFileTypes, pass: string): Promise<AllKeyF
     }
 }
 
+function extractKeysV2(
+    file: KeyFileDecryptedV2 | KeyFileDecryptedV3 | KeyFileDecryptedV4
+): AccessWalletMultipleInput[] {
+    let chainID = avm.getBlockchainAlias()
+    let keys = (file as KeyFileDecryptedV2 | KeyFileDecryptedV3 | KeyFileDecryptedV4).keys
+
+    return keys.map((key) => {
+        // Private keys from the keystore file do not have the PrivateKey- prefix
+        let pk = 'PrivateKey-' + key.key
+        let keypair = keyToKeypair(pk, chainID)
+
+        let keyBuf = keypair.getPrivateKey()
+        let keyHex: string = keyBuf.toString('hex')
+        let paddedKeyHex = keyHex.padStart(64, '0')
+        let mnemonic: string = bip39.entropyToMnemonic(paddedKeyHex)
+
+        return {
+            key: mnemonic,
+            type: 'mnemonic',
+        }
+    })
+}
+
+function extractKeysV5(file: KeyFileDecryptedV5): AccessWalletMultipleInput[] {
+    return file.keys.map((key) => ({
+        key: key.key,
+        type: 'mnemonic',
+    }))
+}
+
+function extractKeysV6(file: KeyFileDecryptedV6): AccessWalletMultipleInput[] {
+    return file.keys.map((key) => ({
+        type: key.type,
+        key: key.key,
+    }))
+}
+
+function extractKeysFromDecryptedFile(file: AllKeyFileDecryptedTypes): AccessWalletMultipleInput[] {
+    switch (file.version) {
+        case '6.0':
+            return extractKeysV6(file as KeyFileDecryptedV6)
+        case '5.0':
+            return extractKeysV5(file as KeyFileDecryptedV5)
+        case '4.0':
+            return extractKeysV2(file as KeyFileDecryptedV4)
+        case '3.0':
+            return extractKeysV2(file as KeyFileDecryptedV3)
+        case '2.0':
+            return extractKeysV2(file as KeyFileDecryptedV2)
+        default:
+            throw 'INVALID_VERSION'
+    }
+}
+
 // Given an array of wallets and a password, return an encrypted JSON object that is the keystore file
 async function makeKeyfile(
     wallets: (AvaHdWallet | SingletonWallet)[],
@@ -296,53 +350,6 @@ async function makeKeyfile(
         keys: keys,
     }
     return file_data
-}
-
-function extractKeysFromDecryptedFile(file: AllKeyFileDecryptedTypes): AccessWalletMultipleInput[] {
-    let chainID = avm.getBlockchainAlias()
-
-    let accessInput: AccessWalletMultipleInput[]
-    let type: KeystoreFileKeyType = 'mnemonic'
-    let version = file.version
-
-    // Convert old version private keys to mnemonic phrases
-    if (['2.0', '3.0', '4.0'].includes(version)) {
-        let keys = (file as KeyFileDecryptedV2 | KeyFileDecryptedV3 | KeyFileDecryptedV4).keys
-
-        accessInput = keys.map((key) => {
-            // Private keys from the keystore file do not have the PrivateKey- prefix
-            let pk = 'PrivateKey-' + key.key
-            let keypair = keyToKeypair(pk, chainID)
-
-            let keyBuf = keypair.getPrivateKey()
-            let keyHex: string = keyBuf.toString('hex')
-            let paddedKeyHex = keyHex.padStart(64, '0')
-            let mnemonic: string = bip39.entropyToMnemonic(paddedKeyHex)
-
-            return {
-                key: mnemonic,
-                type: 'mnemonic',
-            }
-        })
-    } else if (version === '5.0') {
-        let keys = (file as KeyFileDecryptedV5).keys
-        // New versions encrypt the mnemonic so we dont have to do anything
-        accessInput = keys.map((key) => {
-            return {
-                key: key.key,
-                type: 'mnemonic',
-            }
-        })
-    } else {
-        let keys = (file as KeyFileDecryptedV6).keys
-        accessInput = keys.map((key) => {
-            return {
-                type: key.type,
-                key: key.key,
-            }
-        })
-    }
-    return accessInput
 }
 
 export { readKeyFile, makeKeyfile, KEYSTORE_VERSION, extractKeysFromDecryptedFile }
