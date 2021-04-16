fc1da4dd036269bed6f183f1a0ebae654fd9ba10
paOol <paoolkim@gmail.com>
2021-04-16 13:47:26 -0700

    identicon component

--- src/components/misc/Identicon.vue
+++ src/components/misc/Identicon.vue
@@ -1,27 +1,38 @@
 <template>
-    <canvas ref="canvas"></canvas>
+    <div class="identicon">
+        <img :src="src" alt="" :height="diameter" :width="diameter" />
+    </div>
 </template>
-<script>
-import * as jdenticon from 'jdenticon'
 
-export default {
-    props: {
-        hash: {
-            type: String,
-            required: true,
-        },
-    },
-    methods: {
-        generateIdenticon() {
-            let canv = this.$refs['canvas']
-            let cont = canv.getContext('2d')
-            let text = this.payload.toString('hex')
-            jdenticon.drawIcon(cont, text, canv.width)
-        },
-    },
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { toDataUrl } from '@/js/blockies.js'
+
+// import jazzicon from '@metamask/jazzicon'
+// const iconFactoryGen = require('@/js/iconFactory')
+// const iconFactory = iconFactoryGen(jazzicon)
+
+@Component
+export default class Identicon extends Vue {
+    image: HTMLImageElement | null = null
+    src!: string
+
+    @Prop()
+    address!: string
+    @Prop({ default: 40 }) diameter!: number
+
+    created() {
+        this.generateBlockie(this.address)
+    }
+    generateBlockie(address: string) {
+        this.src = toDataUrl(address)
+    }
 }
 </script>
 <style scoped lang="scss">
-canvas {
+.identicon {
+    img {
+        border-radius: 100%;
+    }
 }
 </style>
