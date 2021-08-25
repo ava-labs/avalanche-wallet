a18b957c28714aac8c5df6b7b352d9b6fa02fa7f
Emre Kanatli <kanatliemre@gmail.com>
2021-08-25 17:11:11 +0300

    update test to use dynamic mnemonics

--- tests/js/wallets/MnemonicPhrase.test.ts
+++ tests/js/wallets/MnemonicPhrase.test.ts
@@ -1,11 +1,13 @@
 import MnemonicPhrase from '@/js/wallets/MnemonicPhrase'
-
-const PHRASE =
-    'uncover sight giant angry loud enter heavy receive whip crisp luggage monkey bracket wide diary blanket food gown rail sudden scissors crop afford snake'
+import * as bip39 from 'bip39'
 
 describe('MnemonicPhrase', () => {
     it('can encrypt and decrypt', () => {
-        let phrase = new MnemonicPhrase(PHRASE)
-        expect(phrase.getValue()).toEqual(PHRASE)
+        const NUM_ITERATION = 10000
+        for (var i = 0; i < NUM_ITERATION; i++) {
+            let phraseRaw = bip39.generateMnemonic(256)
+            let phrase = new MnemonicPhrase(phraseRaw)
+            expect(phrase.getValue()).toEqual(phraseRaw)
+        }
     })
 })
