5ab1a896ac9c724d0d95e64ddd09ec49cda2d12f
Patrick O'Grady <prohb125@gmail.com>
2021-11-29 23:37:57 -0800

    never return the memo to the UI

--- src/services/history/utils.ts
+++ src/services/history/utils.ts
@@ -1,13 +1,4 @@
-import { Buffer } from 'avalanche'
-import urlRegex from 'url-regex'
-
 export default function getMemoFromByteString(memo: string) {
-    const memoText = new Buffer(memo, 'base64').toString('utf8')
-
-    // Bug that sets memo to empty string (AAAAAA==) for some tx types
-    if (!memoText.length || memo === 'AAAAAA==') return null
-
-    // If memo contains URL do not show memo
-    if (urlRegex({ exact: false, strict: false }).test(memoText)) return null
-    return memoText
+  // Never return the memo to the UI
+  return null
 }
