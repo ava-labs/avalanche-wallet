ee0a8428570e61fe695a053fd445626824562299
Gergely Lovas <36536513+gergelylovas@users.noreply.github.com>
2021-12-21 15:53:04 +0100

    feat: update metadata (#282)
    * feat: update metadata
    * feat: update og image

--- src/views/access/Access.vue
+++ src/views/access/Access.vue
@@ -11,6 +11,36 @@
         </b-container>
     </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+@Component({
+    metaInfo: () => {
+        const description =
+            'Access your stored crypto assets in a simple fashion, yet highly secure, and non-custodial fashion. Your Avalanche wallet is a few clicks away!'
+        return {
+            meta: [
+                {
+                    vmid: 'description',
+                    name: 'description',
+                    content: description,
+                },
+                {
+                    vmid: 'og:description',
+                    name: 'description',
+                    content: description,
+                },
+                {
+                    vmid: 'og:title',
+                    name: 'og:title',
+                    content: 'Access Stored Crypto Assets | Avalanche Wallet',
+                },
+            ],
+            title: 'Access Stored Crypto Assets',
+        }
+    },
+})
+export default class Access extends Vue {}
+</script>
 <style scoped lang="scss">
 @use "../../main";
 
