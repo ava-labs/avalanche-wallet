506618946378c2435f78ace0e5d6e5a10035e186
Emre Kanatli <kanatliemre@gmail.com>
2022-06-23 18:49:29 -0500

    chore: update language

--- src/components/modals/ExportAvaxCsvModal.vue
+++ src/components/modals/ExportAvaxCsvModal.vue
@@ -1,7 +1,7 @@
 <template>
     <modal ref="modal" title="Export AVAX Transfers" class="modal_main">
         <div class="csv_modal_body">
-            <p>Only X chain AVAX transactions will be exported.</p>
+            <p>Export AVAX transactions including cross chain transfers on X,P and C chains.</p>
             <p class="err" v-if="error">{{ error }}</p>
             <v-btn
                 class="button_secondary"
