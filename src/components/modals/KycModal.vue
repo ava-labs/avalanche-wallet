7da6dc7c238bdb4c6f3315ba39a1d90ebb0f6833
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-08-18 12:43:31 +0100

    match dark theme with kyc modal

--- src/components/modals/KycModal.vue
+++ src/components/modals/KycModal.vue
@@ -83,7 +83,103 @@ export default class KycModal extends Vue {
     onthemechange(val: string) {
         if (val === 'night') {
             this.background =
-                'body {background-color: var(--secondary-color) !important; min-height: 450px !important;} .line {background-color: black !important;}'
+                ".step.active .line, .step.active .bullet:before, .radio-item .checkmark:after, .step.active.pending .bullet:before {\
+    background-color: #149ded;\
+}\
+.accent {\
+    color: #149ded;\
+}\
+.step .title, .title  {\
+    color: #f5f5f5;\
+}\
+.step.active .title {\
+    color: #149ded;\
+    font-size: 14px;\
+    font-weight: 400;\
+}\
+section {\
+    border-radius: 12px;\
+    box-shadow: rgb(0 0 0 / 10%) 0px 0px 5px;\
+    background-color: #1e293b;\
+}\
+p , h3, h2, label, .markdown-instructions li , .markdown-instructions p,.line-form .line-form-item > .phone-input,\
+.line-form .line-form-item > input{\
+    color : #f5f5f5 !important;\
+    font-size : 14px;\
+}\
+.document-examples, .upload {\
+    gap : 10px;\
+}\
+.upload-payment-item {\
+    margin : 0px;\
+}\
+.upload-payment-item .upload-item , .mobile-button{\
+    border: 1px solid rgba(203, 213, 225, 0.12);\
+    border-radius: 7px;\
+    box-shadow: rgb(0 0 0 / 10%) 0px 0px 5px;\
+}\
+ .mobile-button h3{\
+    color : #149ded !important;\
+ }\
+ button.submit,\
+button[type='submit'] {\
+    border-radius: 12px;\
+    background-color: transparent;\
+    background-image: none;\
+    color: #149ded;\
+    border: 1px solid #149ded;\
+}\
+button:active:not(:disabled):not(.disabled),\
+button:hover:not(:disabled):not(.disabled):not(:active) {\
+    box-shadow: none;\
+}\
+button.submit:active:not(:disabled),\
+button.submit:hover:not(:disabled):not(.disabled):not(:active),\
+button[type='submit']:active:not(:disabled),\
+button[type='submit']:hover:not(:disabled):not(.disabled):not(:active) {\
+     background-image: none;\
+}\
+button {\
+    border-radius: 12px;\
+    background-color: transparent;\
+    font-weight: 600;\
+    text-align: center;\
+    color: #149ded;\
+    border: 1px solid #149ded;\
+}\
+.line-form .line-form-item > span {\
+    border-bottom: none;\
+}\
+button.submit .arrow, button[type=submit] .arrow {\
+    margin-right: 0;\
+    margin-left: 5px;\
+}\
+button .arrow {\
+    margin-right: 5px;\
+}\
+.upload-item h4.requiredDoc:after {\
+    color: #149ded;\
+}\
+.popup {\
+    background: #1e293b !important;\
+}\
+.popup .message-content p {\
+    color: #f5f5f5 !important;\
+}\
+.step.pending .bullet {\
+    background-color: #f5f5f5;\
+    background-image: none;\
+    border-color: #f5f5f5;\
+}\
+.step.pending .line , .step.active .line, .step.success .line{\
+    background-color: #149ded;\
+}\
+.step.success .bullet {\
+    background-color: #149ded;\
+    border-color: #f5f5f5;\
+}\
+"
+            // 'body {background-color: var(--secondary-color) !important; min-height: 450px !important;} .line {background-color: black !important;}'
         } else {
             this.background = 'body {background-color: red !important;}'
         }
@@ -167,7 +263,7 @@ export default class KycModal extends Vue {
 .modal_main::v-deep {
     .modal_body {
         width: 70%;
-        max-width: 650px;
+        max-width: 750px;
         height: auto !important;
         /* min-height: 450px !important; */
         border-radius: var(--border-radius-sm) !important;
@@ -179,9 +275,6 @@ export default class KycModal extends Vue {
             left: 50%;
             top: 50%;
             transform: translate(-50%, -50%);
-        }
-        .line {
-            background-color: black !important;
         }
     }
 
@@ -224,6 +317,72 @@ h1 {
     }
 }
 
+.popup {
+    background: #1e293b;
+}
+.popup .message-content p {
+    color: #f5f5f5;
+}
+/* .steps {
+}
+.step .activ {
+}
+
+.step.active .line {
+    background-color: red;
+}
+.bullet::before {
+    background-color: black;
+}
+.title {
+    color: white;
+}
+
+.step .title {
+    color: #f5f5f5;
+}
+.step.active .title {
+    color: #149ded;
+} */
+/* button.submit,
+button[type='submit'] {
+    border-radius: 12px;
+    background-color: transparent;
+    background-image: none;
+    color: #149ded;
+    border: 1px solid #149ded;
+}
+.upload-payment-item .upload-item {
+    border: 1px solid rgba(203, 213, 225, 0.12);
+    border-radius: 7px;
+    box-shadow: rgb(0 0 0 / 10%) 0px 0px 5px;
+    background-color: var(--white-color);
+}
+
+section {
+    border-radius: 7px;
+    box-shadow: rgb(0 0 0 / 10%) 0px 0px 5px;
+    background-color: #1e293b;
+} */
+.step.active.pending .bullet:before {
+    background-color: var(--orange-color);
+}
+.line-form .line-form-item > .phone-input,
+.line-form .line-form-item > input {
+    color: red;
+}
+.line-form .line-form-item > span {
+    border-bottom: none;
+}
+
+button {
+    border-radius: 12px;
+    background-color: transparent;
+    font-weight: 600;
+    text-align: center;
+    color: #7c8ab5;
+    border: 1px solid #149ded;
+}
 input {
     color: var(--primary-color);
     background-color: var(--bg-light);
