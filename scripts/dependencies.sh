8f7b1282f8872b1240da20eaca6a8026cbb5e6a6
peak3d <pfau@peak3d.de>
2022-11-21 09:04:21 +0100

    Various CI changes

---
+++ scripts/dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if ! grep -q 'file:../camino-wallet-sdk/package' package.json; then
+  echo "No file dependencies required"
+  exit 0
+fi
+
+# Prepare caminojs
+cd ..
+git clone https://github.com/chain4travel/caminojs --depth 1 -bkopernikus
+cd caminojs
+yarn
+yarn build
+yarn pack
+tar -xzf c4tplatform-caminojs-v*.tgz
+
+# prepare camino-wallet-sdk
+cd ..
+git clone https://github.com/chain4travel/camino-wallet-sdk --depth 1 -bdev
+cd camino-wallet-sdk
+yarn
+yarn build
+yarn pack
+tar -xzf c4tplatform-camino-wallet-sdk-v*.tgz
+sed -i 's/file:..\/caminojs\/package/file:..\/..\/caminojs\/package/1' package/package.json
+
+# Finally restore previous directory
+cd ../camino-wallet
