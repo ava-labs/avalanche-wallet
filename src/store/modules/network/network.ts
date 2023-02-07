f11994223cc09c2d38d4a8ab70c2ee2b50742af1
aeddaqqa <achrafeddaqqaq@gmail.com>
2023-02-07 16:38:31 +0100

    set columbus ad default network

--- src/store/modules/network/network.ts
+++ src/store/modules/network/network.ts
@@ -211,7 +211,7 @@ const network_module: Module<NetworkState, RootState> = {
             try {
                 let isSet = await dispatch('loadSelectedNetwork')
                 if (!isSet) {
-                    await dispatch('setNetwork', state.networks[0])
+                    await dispatch('setNetwork', state.networks[1])
                 }
                 return true
             } catch (e) {
