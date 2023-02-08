52bb12698c492c8f8c50a97b88420c3ec3c2d428
jamesckhsiao <james.h@cryptophet.com>
2023-02-08 13:55:15 +0800

    test(e2e): modify plugin to select test target by env vars

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'cypress'
 
+import parseEnvPlugin from './cypress/plugins'
+
 export default defineConfig({
-  nodeVersion: 'system',
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+    e2e: {
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            return parseEnvPlugin(on, config)
+        },
+        baseUrl: 'https://localhost:5000/',
     },
-    baseUrl: 'https://localhost:5000/',
-  },
 })
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -3,23 +3,44 @@
  * create a object here, dynamically change it based on various environment variables
  * and pass it to cypress just before it starts.
  */
-const CYPRESS_BASE_CONFIG = {
-    nodeVersion: 'system',
-}
 
 const CYPRESS_CONFIG_PROD = {
-    ...CYPRESS_BASE_CONFIG,
     baseUrl: 'https://wallet.camino.network/',
 }
 
+const CYPRESS_CONFIG_COLUMBUS = {
+    baseUrl: 'https://playground.suite.camino.foundation/',
+}
+
 const CYPRESS_CONFIG_LOCAL = {
-    ...CYPRESS_BASE_CONFIG,
     baseUrl: `http://localhost:3000/`,
 }
 
+const mergeConfig = (config, override) => ({
+    ...config,
+    ...override,
+})
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = () => {
-    return process.env.runProduction ? CYPRESS_CONFIG_PROD : CYPRESS_CONFIG_LOCAL
+export default (on, config) => {
+    const testEnv = process.env.TEST_ENV?.toLowerCase()
+    console.log(`Will testing on ${testEnv} ...`)
+
+    let cypressConfig = mergeConfig(config, CYPRESS_CONFIG_LOCAL)
+    switch (testEnv) {
+        case 'columbus':
+            console.info('Testing on Columbus')
+            cypressConfig = mergeConfig(config, CYPRESS_CONFIG_COLUMBUS)
+            break
+        case 'prod':
+        case 'production':
+            cypressConfig = mergeConfig(config, CYPRESS_CONFIG_PROD)
+            break
+        case 'local':
+        case 'dev':
+        default:
+            break
+    }
+    return cypressConfig
 }
