fdf73c3cdb3600638ad56bdf5e04a95393919780
jamesckhsiao <james.h@cryptophet.com>
2023-02-10 16:06:13 +0800

    test(e2e): complete normal case of x to c

--- cypress/e2e/cross-chain/cross_chain_x_to_c.cy.ts
+++ cypress/e2e/cross-chain/cross_chain_x_to_c.cy.ts
@@ -1,9 +1,17 @@
-import { BN, bnToAvaxC, bnToAvaxX, bnToBigAvaxC, bnToBigAvaxX, GasHelper } from '@c4tplatform/camino-wallet-sdk'
+import {
+  BN,
+  bnToAvaxC,
+  bnToAvaxX,
+  bnToBigAvaxC,
+  bnToBigAvaxX,
+  GasHelper,
+  Big,
+} from '@c4tplatform/camino-wallet-sdk'
 
 describe('Cross chain: X to C', { tags: ['@cross-chain'] }, () => {
   context('normal cases: ', () => {
     beforeEach(() => {
-      cy.loginWalletWith('privateKey')
+      cy.loginWalletWith('privateKey', 'privateKeyCrossChain')
       // Switch to cross chain
       cy.switchToWalletFunctionTab('Cross Chain')
       // Make sure successfully switched to the Cross Chain tab
@@ -15,27 +23,48 @@ describe('Cross chain: X to C', { tags: ['@cross-chain'] }, () => {
 
     it('export CAM from X to C', () => {
       cy.get('label').contains('Source Chain').siblings('select').first().as('selectSource')
-      cy.get('label').contains('Destination Chain').siblings('select').first().as('selectDestination')
+      cy.get('label')
+        .contains('Destination Chain')
+        .siblings('select')
+        .first()
+        .as('selectDestination')
       // Switch source and destination chains
-      cy.get('@selectSource').invoke('val').then(value => {
-        if (value !== 'X') {
-          cy.get('@selectSource').select('X')
-        }
-      })
-      cy.get('@selectDestination').invoke('val').then(value => {
-        if (value !== 'C') {
-          cy.get('@selectDestination').select('C')
-          cy.wait('@apiBaseFee').then(intercept => {
-            const hexBaseFee = intercept.response?.body.result
-            const bnBaseFee = new BN(hexBaseFee.substring(2), 'hex')
-            cy.wrap(bnBaseFee).as('bnBaseFee')
-          })
-        }
-      })
+      cy.get('@selectSource')
+        .invoke('val')
+        .then((value) => {
+          if (value !== 'X') {
+            cy.get('@selectSource').select('X')
+          }
+        })
+      cy.get('@selectDestination')
+        .invoke('val')
+        .then((value) => {
+          if (value !== 'C') {
+            cy.get('@selectDestination').select('C')
+            cy.wait('@apiBaseFee').then((intercept) => {
+              const hexBaseFee = intercept.response?.body.result
+              const bnBaseFee = new BN(hexBaseFee.substring(2), 'hex')
+              cy.wrap(bnBaseFee).as('bnBaseFee')
+            })
+          }
+        })
+
+      // enter amount to transfer
+      cy.get('.swap_form .avax_input input[type="number"]').type('0.001')
+
+      // initial balances
+      cy.get('.chain_card .balance')
+        .first()
+        .invoke('text')
+        .then((balance) => cy.wrap(balance).as('initialXBalance'))
+      cy.get('.chain_card .balance')
+        .last()
+        .invoke('text')
+        .then((balance) => cy.wrap(balance).as('initialCBalance'))
 
       // the default tx fee of X/P chain is intercepting from rpc on network changed
       // from source chain
-      cy.get<string>('@txFee').then(txFee => {
+      cy.get<string>('@txFee').then((txFee) => {
         // check export fee
         console.debug('txFee: ', bnToAvaxX(new BN(txFee)))
         cy.get('div')
@@ -47,10 +76,12 @@ describe('Cross chain: X to C', { tags: ['@cross-chain'] }, () => {
         // use (camino-wallet-sdk).GasHelper to estimate the tx fee of importing to C chain
         const importTxFee = GasHelper.estimateImportGasFeeFromMockTx(1, 1)
         console.debug('import tx fee: ', importTxFee)
-        cy.get<BN>('@bnBaseFee').then(baseFee => {
-          console.debug('C base fee: ', baseFee.toString())
+        cy.get<BN>('@bnBaseFee').then((baseFee) => {
+          console.debug('C base fee(BN): ', baseFee.toString())
           // `getBaseFeeRecommended` function will increase the base fee with 25% up
-          const feeWei = baseFee.add(baseFee.mul(new BN(25)).div(new BN(100))).mul(new BN(importTxFee))
+          const feeWei = baseFee
+            .add(baseFee.mul(new BN(25)).div(new BN(100)))
+            .mul(new BN(importTxFee))
           console.debug('C import fee: ', bnToAvaxC(feeWei))
           cy.get('div')
             .contains('Import Fee')
@@ -59,27 +90,114 @@ describe('Cross chain: X to C', { tags: ['@cross-chain'] }, () => {
           cy.wrap(bnToBigAvaxC(feeWei)).as('importFee')
         })
 
-        // check totoal fee
-        cy.get('.swap_form')
-          .find('input[type="number"]')
-          .invoke('val')
-          .then(amount => {
-            console.debug(`input amount: "${amount}"`)
-            cy.get<string>('@txFee')
-              .then(exportFee => {
-                return bnToBigAvaxX(new BN(exportFee)).plus(String(amount || 0))
-              })
+        cy.get<string>('@txFee')
+          .then((exportFee) => {
+            console.debug('exportFee: ', exportFee?.toString())
+            cy.get<BigJs.Big>('@importFee').then((importFee) =>
+              importFee.plus(bnToBigAvaxX(new BN(exportFee)))
+            )
           })
-          .then(sum => {
-            console.debug('sum: ', sum?.toString())
-            cy.get<BigJs.Big>('@importFee').then(importFee => importFee.plus(String(sum)))
-          })
-          .then(totalFee => {
+          .then((totalFee) => {
             console.debug('totalFee: ', totalFee?.toString())
             cy.get('div')
               .contains('Total')
               .find('span')
               .should('have.text', `${totalFee?.toString()} CAM`)
+          })
+
+        // send tx
+        // click CONFIRM button
+        cy.get('div.fees:has(> h4) + div button').click()
+        // click TRANSFER button
+        cy.get('[data-cy="submit"]').click()
+        // listening rpcs
+        cy.intercept('POST', '**/ext/bc/X').as('apiExportX')
+        cy.intercept('POST', '**/ext/bc/C/avax').as('apiImportC')
+        // wait `avm.issueTx` to get txID
+        cy.wait('@apiExportX').then((intercept) => {
+          if (intercept.request.body.method === 'avm.issueTx') {
+            const txID = intercept.response?.body.result.txID
+            cy.get('.tx_state_card').first().get('.data_row > p').first().should('have.text', txID)
+          }
+        })
+        // wait `avm.getTxStatus` to get the tx status
+        // it might return twice with statuses 'Processing' and 'Accepted
+        cy.wait('@apiExportX').then((intercept) => {
+          if (intercept.request.body.method === 'avm.getTxStatus') {
+            const txStatus = intercept.response?.body.result.status
+            // UI won't display 'Processing', only 'Waiting'
+            let expectedStatus = txStatus
+            if (txStatus === 'Processing') {
+              expectedStatus = 'Waiting'
+            }
+            cy.get('.tx_state_card')
+              .first()
+              .get('.data_row > p')
+              .last()
+              .should('have.text', expectedStatus)
+          }
+        })
+        // wait `avax.issueTx` to get txID
+        cy.wait('@apiImportC').then((intercept) => {
+          if (intercept.request.body.method === 'avax.issueTx') {
+            const txID = intercept.response?.body.result.txID
+            cy.get('.tx_state_card').last().get('.data_row > p').first().should('have.text', txID)
+          }
+        })
+        // wait `avax.getAtomicTxStatus` to get the tx status
+        cy.wait('@apiImportC').then((intercept) => {
+          if (intercept.request.body.method === 'avax.getAtomicTxStatus') {
+            const txStatus = intercept.response?.body.result.status
+            cy.get('.tx_state_card')
+              .last()
+              .get('.data_row > p')
+              .last()
+              .should('have.text', txStatus)
+          }
+        })
+
+        // check 'Transfer Completed' message
+        cy.get('.complete > h4').should('have.text', 'Transfer Completed')
+
+        // check the balances on X chain
+        cy.get('.confirmation_val > p')
+          .invoke('text')
+          .then((amountWithSymbol) => amountWithSymbol.replace(/(\s)?CAM/, ''))
+          .as('amount')
+        cy.get<string>('@initialXBalance')
+          .then((initialBalance) => {
+            console.debug('initial X balance: ', initialBalance)
+            cy.get<string>('@amount').then((amount) => {
+              // initial X balance - amount
+              return new Big(initialBalance).minus(amount)
+            })
+          })
+          .then((bigBalance) => {
+            cy.get<string>('@txFee').then((exportFee) =>
+              // (initial X balance - amount) - export fee
+              new Big(bigBalance).minus(bnToBigAvaxX(new BN(exportFee)))
+            )
+          })
+          .then((bigBalance) => {
+            cy.get<BigJs.Big>('@importFee').then((importFee) =>
+              // (initial X balance - amount - export fee) - import fee
+              new Big(bigBalance).minus(importFee)
+            )
+          })
+          .then((expectedBalance) => {
+            cy.get('.chain_card .balance').first().should('have.text', expectedBalance.toString())
+          })
+        // check the balances on C chain
+        cy.get<string>('@initialCBalance')
+          .then((initialBalance) => {
+            console.debug('initial C balance: ', initialBalance)
+            cy.get<string>('@amount').then((amount) => {
+              // initial C balance + amount
+              return new Big(initialBalance).plus(amount)
+            })
+          })
+          .then((expectedBalance) => {
+            cy.get('.chain_card .balance').last().should('have.text', expectedBalance.toString())
           })
       })
     })
