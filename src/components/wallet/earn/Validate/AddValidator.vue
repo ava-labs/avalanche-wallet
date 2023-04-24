82588ebf69d4ad82e08a232343fedcf054b9b20a
Daniel Lopez <danilocha1@gmail.com>
2023-04-24 09:23:15 -0500

    fix prettier errors

--- src/components/wallet/earn/Validate/AddValidator.vue
+++ src/components/wallet/earn/Validate/AddValidator.vue
@@ -15,14 +15,21 @@
                             <p class="desc">
                                 {{ $t('earn.validate.duration.desc') }}
                             </p>
-                            <DateForm @change_end="setEnd" :typeDateForm="'validatorDateForm'"></DateForm>
+                            <DateForm
+                                @change_end="setEnd"
+                                :typeDateForm="'validatorDateForm'"
+                            ></DateForm>
                         </div>
                         <div v-if="isMultiSig" style="margin: 30px 0">
                             <h4>{{ $t('earn.validate.transaction_duration.label') }}</h4>
                             <p class="desc">
                                 {{ $t('earn.validate.transaction_duration.desc') }}
                             </p>
-                            <DateForm @change_end="setTransactionEnd" :typeDateForm="'transactionDateForm'" tx="true"></DateForm>
+                            <DateForm
+                                @change_end="setTransactionEnd"
+                                :typeDateForm="'transactionDateForm'"
+                                tx="true"
+                            ></DateForm>
                         </div>
                         <div style="margin: 30px 0">
                             <h4>{{ $t('earn.validate.amount.label') }}</h4>
