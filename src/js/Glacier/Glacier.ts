aab39bd4ac19cde97def5196686cefd7eed28d87
Lavinia Talpas <laviniatalpas@Lavinias-MacBook-Pro.local>
2023-12-05 19:51:10 +0200

    update glacier api link to production

--- src/js/Glacier/Glacier.ts
+++ src/js/Glacier/Glacier.ts
@@ -1,6 +1,6 @@
 import { Glacier } from '@avalabs/glacier-sdk'
 
 const api = new Glacier({
-    BASE: 'https://glacier-api-dev.avax.network',
+    BASE: 'https://glacier-api.avax.network',
 })
 export default api
